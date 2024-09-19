e30899eaa9a6c73c64a7d160a6e8c66cf62033b4
shubhamaaaa <130844125+shubhamaaaa@users.noreply.github.com>
2024-09-19 17:48:25 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AbortedDeferredError, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Collection from './pages/collection';
+import Collection from './pages/Collection';
 import About from './pages/About';
 import Contact from './pages/contact';
 import Product from './pages/product';
